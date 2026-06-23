da67515c26909cef502e90bed6f4892e6817a211
Muhammad Ahsan <ahsanchilll56@gmail.com>
2026-06-24 00:42:20 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="/favicon-new.png" />
       </Head>
 
       <Component {...pageProps} />
